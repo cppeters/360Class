b1792c441aa186f0ada1740ec47f588ad404f8c0
lizMillerCoder12 <millerElizabeth1991@gmail.com>
2016-05-25 20:56:47 -0700

    Removing the path for csv file

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -16,7 +16,7 @@ import model.*;
 public class Main {
 	
 	/*		File Names  	*/
-	private static final String USER_FILE = "360Class/User.csv";
+	private static final String USER_FILE = "User.csv";
 	private static final String CONTEST_FILE = "Contests.csv";
 	private static final String ENTRY_FILE = "Entries.csv";
 	
